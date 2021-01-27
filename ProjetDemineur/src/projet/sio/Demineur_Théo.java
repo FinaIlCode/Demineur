a5008694ac374e6082765da890266b9f2c6cbfb9
Théo <theo.gaming@outlook.fr>
2021-01-27 23:33:59 +0100

    Affichage fait, Bombe ok, Test bombe coin début

--- ProjetDemineur/src/projet/sio/Demineur_Théo.java
+++ ProjetDemineur/src/projet/sio/Demineur_Théo.java
@@ -10,23 +10,215 @@ package projet.sio;
  * @author delot
  */
 public class Demineur_Théo {
-  
+
     /**
-    * retourne true s'il existe un "M" à cet indice
-    *
-    * @author Thierry
-    *
-    * @param tDemineur tableau de la grille du démineur
-    * @param indice indice dans tDemineur. Doit être compris entre 0 et 99
-    *
-    * @return
-    *    true il y a une mine à indice.
-    *    false pas de mine à indice.
-    *
-    */
-    public static boolean isMine(String[] tDemineur, int indice){
-       if (tDemineur[indice] == "M" ) return true;
-       return false;
+     * retourne true s'il existe un "M" à cet indice
+     *
+     * @param tDemineur tableau de la grille du démineur
+     * @return true il y a une mine à indice.
+     * false pas de mine à indice.
+     * @author Thierry
+     */
+    public static void affiche(char[] tLettre, char[] tDemineur) {
+        System.out.println("  1 2 3 4 5 6 7 8 9 10");
+        int k = 0;
+        int l = 10;
+        int compteur=0;
+        char espace = ' ';
+        for (int i = 0; i < 10; i++) {
+            System.out.print(tLettre[i]);
+            System.out.print(" ");
+            for (int j = k; j < l; j++) {
+                if (tDemineur[j] == 'M') {
+                    System.out.print("M ");
+                } else {
+                    if(tDemineur[compteur] == '1' ||tDemineur[compteur] == '2' ||tDemineur[compteur] == '3' ||tDemineur[compteur] == '4' ||tDemineur[compteur] == '5' ||tDemineur[compteur] == '6' ||tDemineur[compteur] == '7' ||tDemineur[compteur] == '8' ||tDemineur[compteur] == '9' ){
+                        System.out.print(tDemineur[compteur] + " ");
+                    } else {
+                        if (tDemineur[compteur]=='X') {
+                            System.out.print(tDemineur[compteur] + " ");
+                        } else{
+                            if(tDemineur[compteur]=='0'){
+                                System.out.print(espace + espace);
+                            } else {
+                                System.out.print("- ");
+                            }
+                        }
+                    }
+                }
+                compteur++;
+            }
+            k = k + 10;
+            l = k + 10;
+            System.out.println("");
+        }
+
     }
-    
+
+
+    public static boolean isMine(char[] tDemineur, int indice) {
+        indice--;
+        if (tDemineur[indice] == 'M') return true;
+        return false;
+    }
+
+    public static void initParti(char[] tDemineur){
+        int mine = 10;
+        int random;
+        while(mine>0){
+            random = (int)(Math.random()*99);
+            System.out.println(random);
+            tDemineur[random] = 'M';
+            mine--;
+        }
+    }
+
+    public static int position (String coordonne){
+        char lettre;
+        char unite;
+        int position;
+        if(coordonne.length()==2) {
+            lettre = coordonne.charAt(0);
+            position = traitementPosition(lettre);
+            unite = coordonne.charAt(1);
+            position += traitementPosition(unite);
+        } else {
+            lettre = coordonne.charAt(0);
+            position = traitementPosition(lettre);
+            position+=10;
+        }
+
+        return position;
+    }
+
+
+    private static int traitementPosition(char caractere){
+        int valeur = 0;
+        switch(caractere){
+            case 'A':
+                valeur = 0;
+                break;
+            case 'B':
+                valeur = 10;
+                break;
+            case 'C':
+                valeur = 20;
+                break;
+            case 'D':
+                valeur = 30;
+                break;
+            case 'E':
+                valeur = 40;
+                break;
+            case 'F':
+                valeur = 50;
+                break;
+            case 'G':
+                valeur = 60;
+                break;
+            case 'H':
+                valeur = 70;
+                break;
+            case 'I':
+                valeur = 80;
+                break;
+            case 'J':
+                valeur = 90;
+                break;
+            case '1':
+                valeur = 1;
+                break;
+            case '2':
+                valeur = 2;
+                break;
+            case '3':
+                valeur = 3;
+                break;
+            case '4':
+                valeur = 4;
+                break;
+            case '5':
+                valeur = 5;
+                break;
+            case '6':
+                valeur = 6;
+                break;
+            case '7':
+                valeur = 7;
+                break;
+            case '8':
+                valeur = 8;
+                break;
+            case '9':
+                valeur = 9;
+                break;
+
+
+        }
+        return valeur;
+    }
+
+    public static char compteurBombe(char[] tDemineur,int valeurPosition){
+        int cBombe= 0 ;
+        valeurPosition--;
+        //Coins
+        if(valeurPosition == 0 || valeurPosition == 9 || valeurPosition == 90 || valeurPosition == 99 ){
+            if(valeurPosition==0){
+                if (tDemineur[valeurPosition+1]=='M') {
+                    cBombe++;
+                }
+                if (tDemineur[valeurPosition+10]=='M'){
+                    cBombe++;
+                }
+                if (tDemineur[valeurPosition+11]=='M'){
+                    cBombe++;
+                }
+            }
+
+            if(valeurPosition==9){
+                if (tDemineur[valeurPosition-1]=='M') {
+                    cBombe++;
+                }
+                if (tDemineur[valeurPosition+9]=='M'){
+                    cBombe++;
+                }
+                if (tDemineur[valeurPosition+10]=='M'){
+                    cBombe++;
+                }
+            }
+
+            if(valeurPosition==90){
+                if (tDemineur[valeurPosition+1]=='M') {
+                    cBombe++;
+                }
+                if (tDemineur[valeurPosition-10]=='M'){
+                    cBombe++;
+                }
+                if (tDemineur[valeurPosition-11]=='M'){
+                    cBombe++;
+                }
+            }
+
+            if(valeurPosition==99){
+                if (tDemineur[valeurPosition-1]=='M') {
+                    cBombe++;
+                }
+                if (tDemineur[valeurPosition-10]=='M'){
+                    cBombe++;
+                }
+                if (tDemineur[valeurPosition-11]=='M'){
+                    cBombe++;
+                }
+            }
+        }
+        System.out.println(cBombe);
+        char charBombe= intToChar(cBombe);
+        return charBombe;
+    }
+
+    private static char intToChar(int i){
+        String s = ""+i;
+        return s.charAt(0);
+    }
+
 }
--- ProjetDemineur/src/projet/sio/HelloDemineur.java
+++ ProjetDemineur/src/projet/sio/HelloDemineur.java
@@ -5,6 +5,10 @@
  */
 package projet.sio;
 
+import plum.console.Clavier;
+
+import java.awt.*;
+
 /**
  *
  * @author thbogusz
@@ -15,8 +19,31 @@ public class HelloDemineur {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hello Démineur !!");
+
+        char[] tLettre = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',};
+        char[] tDemineur = new char[100];
+
+        Demineur_Théo.initParti(tDemineur);
+        Demineur_Théo.affiche(tLettre, tDemineur);
+        Boolean continuer = true;
+
+        while (continuer) {
+
+            String position = Clavier.lireTexte("Saisissez une position : ");
+            int valeurPosition = Demineur_Théo.position(position);
+
+            if (Demineur_Théo.isMine(tDemineur, valeurPosition)) {
+                tDemineur[valeurPosition] = 'X';
+                Demineur_Théo.affiche(tLettre, tDemineur);
+                System.out.println("Vous avez perdu ! ");
+                continuer = false;
+            } else {
+                tDemineur[valeurPosition]=Demineur_Théo.compteurBombe(tDemineur,valeurPosition);
+                Demineur_Théo.affiche(tLettre, tDemineur);
+            }
+        }
     }
-    
 }
+
+    
+
